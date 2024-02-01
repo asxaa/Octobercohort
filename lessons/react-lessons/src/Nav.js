c072225f96746979872aaf2427c8f3df3696fe8f
asia <asia.elmi1@outlook.com>
2024-02-01 17:38:17 +0000

    working on react

---
+++ lessons/react-lessons/src/Nav.js
@@ -0,0 +1,21 @@
+// rfce - react functional components exports
+
+import React from "react";
+import './App.css'
+
+function Nav(){
+
+    return(
+        <div>
+            <nav>
+                <ul>
+                    <li><a href='https://www.w3schools.com/css/css_border.asp' target='_blank' rel='noreferrer'>Home</a></li>
+                    <li><a href='https://www.w3schools.com/css/css_border.asp' target='_blank' rel='noreferrer'>About</a></li>
+                    <li><a href='https://www.w3schools.com/css/css_border.asp' target='_blank' rel='noreferrer'>Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
