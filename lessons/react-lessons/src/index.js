aaee3bb208e7bed649ecf59799615aedfe5945d1
asia <asia.elmi1@outlook.com>
2024-05-01 10:47:59 +0100

    finishing off final project

--- lessons/react-lessons/src/index.js
+++ lessons/react-lessons/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Mapping/App'
+import './Project 4/index.css';
+import App from './Project 4/App';
+//import App from './Project 3/App.js'
 //import Exercise1 from './Exercises/Exercise1'
-
-
-
+//var destination = document.querySelector('#container');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -12,9 +12,9 @@ root.render(
   </React.StrictMode>
 );
 
-
 //  ./ inside the current folder
 // ../ outside the current folder
 
 // import ReactComponent from './FileName.js'
 // <ReactComponent />
+
